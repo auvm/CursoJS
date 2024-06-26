825381e3c1456ea0fd9fba741da214dfb8663d46
auvm <angelurielvelascom@gmail.com>
2024-06-26 13:15:37 -0600

    add regular expressions folder

---
+++ Expresiones_regulares/caracteres.js
@@ -0,0 +1,9 @@
+/**
+ * Una expresión regular es toda aquella expresión encerrada entre dos diagonales
+ * /hola/ = podría ya ser una expresión.
+ */
+
+//Literales, especiales (meta caracteres)
+//Tipo caracter, las aserciones, agrupamientos/rangos,
+//cuantificadores, unicode proerty espcaes
+ 
